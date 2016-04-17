1648bb6927ac70c5ad39c56715e6b672d7e3e7c2
Arian Ubuntu <asuar078@fiu.edu>
2016-04-17 18:08:57 -0400

    Moved all plots to one def, got noisy graph and power def

--- DSP_Project_2/DSPP2.py
+++ DSP_Project_2/DSPP2.py
@@ -14,54 +14,81 @@ from mpl_toolkits.mplot3d import Axes3D
 np.set_printoptions(precision=3)
 np.set_printoptions(suppress=True)
 
+def plt_graph(signal_arr, title='Title', x_axis='x-axis', y_axis='y-axis'):
+    '''
+    Plot a signal
+    '''
+    plt.style.use('ggplot')
+
+    rang = generate_range(len(signal_arr))
+
+    fig = plt.figure(figsize=(22, 10))
+    graph = fig.add_subplot(1, 1, 1)
+
+    graph.plot(rang, signal_arr)
+    graph.set_title(title, fontsize=35)
+    graph.set_xlabel(x_axis, fontsize=25)
+    graph.set_ylabel(y_axis, fontsize=25)
+    plt.show()
+
+def generate_range(size):
+    return np.arange(0, size*0.1, 0.1)
+
 def generate_noise(low, high, size):
+    '''
+    generates random float values inbetween low high
+    values.
+    return
+        array of random float
+    '''
     return np.random.uniform(low, high, size)
 
-def random_noise_plt(low, high, size):
-    x1 = np.arange(0, size, 1)
-    ran = np.random.uniform(low, high, size)
+def generate_sine_wave(size, frequency):
+    '''
+    generates a sine wave
+    return
+        array of sine wave values
+    '''
+#    samples = np.arange(0, size, 0.01)
+    samples = generate_range(size)
 
-    fig = plt.figure(figsize=(22, 10))
-    noise = fig.add_subplot(1, 1, 1)
+    return sin(2*pi*samples*frequency)
 
-    noise.plot(x1, ran)
+def upper_bound_u(noisy, L):
+    mtrx = np.matrix([noisy])
+    pwr = (mtrx * (mtrx.transpose() / mtrx.size))
+    print('Power = '+ str(float(pwr)))
 
-    plt.show()
+    upper = 1 / ((L+1)*float(pwr))
+    print('Upper bound u = ' + str(upper))
 
-def sine_wave(size, frequency, const):
-    sampling_freq = 6*frequency
+def delay(signal, dly):
+    '''
+    Shift a signal by the value in dly
+    return
+    shifted list
+    '''
+    lst = np.zeros(len(signal))
 
-    samples = np.arange(0, size, sampling_freq)
+    for idx in range(dly, len(signal)):
+        lst[idx] = signal[idx-dly]
+    return lst
 
-    return sin(2*pi*samples*frequency*const)
+if __name__ == '__main__':
+    SIZE = 200
+    FREQUENCY = (1.0/16)
 
-def plot_sine_wave(size, frequency, const):
-    sampling_freq = 6*frequency
+    noise = generate_noise(low=-0.3, high=0.3, size=SIZE)
+    sine_wave = generate_sine_wave(SIZE, FREQUENCY)
+    noisy_wave = noise + sine_wave
 
-    samples = np.arange(0, size, 0.01)
+    upper_bound_u(noisy_wave, 50)
 
-#    wave = sin(2*pi*samples*frequency*const)
-    wave = sin(2*pi*samples)
+    delayed = delay(noisy_wave, 40)
 
-    fig = plt.figure(figsize=(22, 10))
-    sine = fig.add_subplot(1, 1, 1)
+    plt_graph(noisy_wave)
+    plt_graph(delayed)
 
-    sine.plot(samples, wave)
 
-    plt.show()
 
-#random_noise_plt(-3, 3, 200)
-plot_sine_wave(200, 1000, 1)
 
-#fs = 44100
-#t = np.arange(0, 1, 1.0/fs)
-#f0 = 100
-#phi = np.pi/2
-#A = .8
-#x = A * np.sin(2 * np.pi * f0 * t + phi)
-#
-#plt.plot(t, x)
-##plt.axis([0, 0.01, -.8, .8])
-#plt.xlabel('time')
-#plt.ylabel('amplitude')
-#plt.show()
